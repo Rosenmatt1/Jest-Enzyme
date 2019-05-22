573ddf1aba6a9323393bd40a2856b217688649cd
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-22 17:37:04 -0600

    Updated the App.test.js file to import enzyme

--- jest-tester/src/App.test.js
+++ jest-tester/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme from'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
 test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+ 
 });
