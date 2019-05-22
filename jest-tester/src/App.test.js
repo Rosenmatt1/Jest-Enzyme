a39c3965fec608be66c0f83a214ba696f471efa4
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-22 17:41:54 -0600

    Added shallow as an import from enzyme

--- jest-tester/src/App.test.js
+++ jest-tester/src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Enzyme from'enzyme'
+import Enzyme, { shallow } from'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders without crashing', () => {
- 
+ const wrapper = shallow( <App /> )
 });
