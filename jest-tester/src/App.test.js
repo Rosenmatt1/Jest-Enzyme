caf3a91e1dbd52661376cad55916ef280ed11fa0
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-22 22:36:34 -0600

    Used the assertion library jest expectto test if to be truthy

--- jest-tester/src/App.test.js
+++ jest-tester/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
@@ -7,5 +7,6 @@ Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders without crashing', () => {
  const wrapper = shallow( <App /> )
- console.log(wrapper.debug())
-});
+//  console.log(wrapper.debug())
+expect(wrapper).toBeTruthy()
+})
