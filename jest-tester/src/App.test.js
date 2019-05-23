8a23a6adfbbe9bd759d6c929183d69aabb72e2e7
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-23 13:59:48 -0600

    Added html elements to get tests to pass

--- jest-tester/src/App.test.js
+++ jest-tester/src/App.test.js
@@ -6,17 +6,23 @@ import App from './App';
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders without crashing', () => {
- const wrapper = shallow( <App /> )
+ const wrapper = shallow( <App /> );
+ const appComponent = wrapper.find("[data-test='component-app']");
+ expect(appComponent.length).toBe(1);
 //  console.log(wrapper.debug())
-expect(wrapper).toBeTruthy()
+// expect(wrapper).toBeTruthy()
 })
 
 test('renders increment button', () => {
- 
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
 })
 
 test('renders counter display', () => {
-
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
 })
 
 test('counter starts at 0', () => {
